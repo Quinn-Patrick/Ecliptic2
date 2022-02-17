6d09f8699b4ccb7e67439bcfb15cc5e613561b56
Quinn Bierbaum <quinn.bierbaum@gmail.com>
2022-02-17 15:39:28 -0500

    The rock will now hypothetically split when destroyed.

--- Ecliptic 2/Assets/GameAssets/SmallObjects/Asteroids/Asteroid.cs
+++ Ecliptic 2/Assets/GameAssets/SmallObjects/Asteroids/Asteroid.cs
@@ -2,13 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Asteroid : GravitatingBody
+public class Asteroid : GravitatingBody, IDestroyable
 {
     [SerializeField] private float _radius;
+    private GameObject[] _subAsteroids = new GameObject[2];
+    [SerializeField] private bool _splits;
     private new void Start()
     {
         base.Start();
         _transform.localScale = new Vector3(_radius, _radius, 0f);
         _body.mass = 1000 * _radius * _radius;
+        if (!_splits) return;
+        if (_radius >= 0.125)
+        {
+            for (int i = 0; i < _subAsteroids.Length; i++)
+            {
+                _subAsteroids[i] = Instantiate(this.gameObject);
+                _subAsteroids[i].GetComponent<Asteroid>().SetRadius(_radius / 3);
+                _subAsteroids[i].SetActive(false);
+            }
+        }
+    }
+
+    public void SetRadius(float rad)
+    {
+        _radius = rad;
+    }
+
+    public void Defeated()
+    {
+        if (!_splits)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        _subAsteroids[0].transform.position = _transform.position + new Vector3(_radius/2, _radius/2, 0);
+        _subAsteroids[0].SetActive(true);
+        _subAsteroids[1].transform.position = _transform.position - new Vector3(_radius / 2, _radius / 2, 0);
+        _subAsteroids[1].SetActive(true);
+        this.gameObject.SetActive(false);
+        Destroy(this.gameObject);
     }
 }
